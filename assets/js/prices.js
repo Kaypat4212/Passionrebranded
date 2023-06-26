2bfda7a981cee9c15dee4ee35badf94295a79fff
patrick ezike <patrickezike66@gmail.com>
2023-06-26 07:30:35 +0100

    updates

--- assets/js/prices.js
+++ assets/js/prices.js
@@ -20,38 +20,46 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     };
   
-    // Display coins data
-    const displayCoinsData = (coinsData) => {
-      const pricesContainer = document.getElementById('prices-container');
-      coinsData.forEach((coin, index) => {
-        const priceDiv = document.createElement('div');
-        priceDiv.classList.add(`price${index + 1}`);
+  // Display coins data
+const displayCoinsData = (coinsData) => {
+  const pricesContainer = document.getElementById('prices-container');
+  coinsData.forEach((coin, index) => {
+    const priceDiv = document.createElement('div');
+    priceDiv.classList.add(`price${index + 1}`);
+    priceDiv.classList.add('coin-price'); // Add a CSS class for styling
   
-        const coinImage = document.createElement('img');
-        coinImage.setAttribute('src', coin.image);
-        coinImage.setAttribute('alt', `${coin.name} logo`);
-        coinImage.setAttribute('width', '23');
-        coinImage.setAttribute('height', '23');
-        coinImage.setAttribute('data-aos', 'flip-left');
-        priceDiv.appendChild(coinImage);
-        priceDiv.innerHTML += '<br/>';
-  
-        const coinName = document.createElement('small');
-        coinName.setAttribute('data-aos', 'zoom-in-up');
-        coinName.textContent = coin.name.toUpperCase();
-        priceDiv.appendChild(coinName);
-        priceDiv.innerHTML += '<br/>';
-  
-        const coinPrice = document.createElement('small');
-        coinPrice.setAttribute('data-aos', 'zoom-in-up');
-        coinPrice.textContent = `$${coin.current_price}`;
-        priceDiv.appendChild(coinPrice);
-        priceDiv.innerHTML += '<br/>';
-  
-        pricesContainer.appendChild(priceDiv);
-      });
-    };
-  
-    fetchCoinsData();
+    const coinImage = document.createElement('img');
+    coinImage.setAttribute('src', coin.image);
+    coinImage.setAttribute('alt', `${coin.name} logo`);
+    coinImage.setAttribute('width', '23');
+    coinImage.setAttribute('height', '23');
+    coinImage.setAttribute('data-aos', 'flip-left');
+    priceDiv.appendChild(coinImage);
+    priceDiv.innerHTML += '<br/>';
+
+    const coinName = document.createElement('small');
+    coinName.setAttribute('data-aos', 'zoom-in-up');
+    coinName.textContent = coin.name.toUpperCase();
+    coinName.classList.add('coin-name'); // Add a CSS class for styling
+    priceDiv.appendChild(coinName);
+    priceDiv.innerHTML += '<br/>';
+
+    const coinPrice = document.createElement('small');
+    coinPrice.setAttribute('data-aos', 'zoom-in-up');
+    coinPrice.textContent = `$${coin.current_price}`;
+    coinPrice.classList.add('coin-price'); // Add a CSS class for styling
+    priceDiv.appendChild(coinPrice);
+    priceDiv.innerHTML += '<br/>';
+
+    pricesContainer.appendChild(priceDiv);
   });
-  
+};
+
+// Fetch coins data
+fetchCoinsData()
+  .then(coinsData => {
+    displayCoinsData(coinsData);
+  })
+  .catch(error => {
+    console.error(error);
+  })})
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fcfd98235ddb15317790afac43f7645a84722d11',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fcfd98235ddb15317790afac43f7645a84722d11',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

a1fb13f7e6eb51986d2cef1b58ae96341a1e58c9
patrick ezike <patrickezike66@gmail.com>
2023-11-27 15:31:06 +0100

    updates

--- subscribe.php
+++ subscribe.php
@@ -4,31 +4,45 @@
 $subscriberEmail = $_POST['email'];
 $name = $_POST['name'];
 
-// Sanitize email address
+// Sanitize inputs
 $subscriberEmail = filter_var($subscriberEmail, FILTER_SANITIZE_EMAIL);
+$name = filter_var($name, FILTER_SANITIZE_STRING);
 
 // Validate email address
 if (!filter_var($subscriberEmail, FILTER_VALIDATE_EMAIL)) {
-  echo 'Invalid email address.';
-  exit;
+    echo 'Invalid email address.';
+    exit;
 }
+
+// Read the HTML content for the welcome email
+$message = file_get_contents('send_newsletter-subscriber.html');
+
+// Replace placeholders in the HTML content with actual values
+$message = str_replace('{name}', $name, $message);
 
 // Send welcome email to subscriber
 $subject = 'Welcome to the Kayxchange newsletter!';
-$message = file_get_contents('/send_newsletter-subscriber.html');
-$headers = 'From: info@kayxchange.net';
+$headers = 'From: info@kayxchange.net' . "\r\n" .
+    'Content-Type: text/html; charset=UTF-8' . "\r\n";
 
-mail($subscriberEmail, $subject, $message, $headers);
+if (mail($subscriberEmail, $subject, $message, $headers)) {
+    echo 'Welcome email sent successfully.';
+} else {
+    echo 'Error sending welcome email.';
+    exit;
+}
 
 // Send email notification to admin
 $adminEmail = 'info@kayxchange.net';
-$subject = 'New subscriber to Kayxchange newsletter!';
-$message = 'A new subscriber has subscribed to your Kayxchange newsletter: ' . $subscriberEmail;
-$headers = 'From: info@kayxchange.net';
+$adminSubject = 'New subscriber to Kayxchange newsletter!';
+$adminMessage = 'A new subscriber has subscribed to your Kayxchange newsletter: ' . $name . ' - ' . $subscriberEmail;
+$adminHeaders = 'From: info@kayxchange.net' . "\r\n" .
+    'Content-Type: text/plain; charset=UTF-8' . "\r\n";
 
-mail($adminEmail, $name, $subject, $message, $headers);
+mail($adminEmail, $adminSubject, $adminMessage, $adminHeaders);
 
 // Redirect the user to the thank you page
 header('Location: Subscribed.html');
+exit;
 
 ?>
